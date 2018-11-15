f79d2149da2ff9cfe0fc3cc0b83e6a5454c516fe
hdy <378759617@qq.com>
2018-11-15 21:59:58 +0800

    init commit

--- README.md
+++ README.md
@@ -1,2 +1,4 @@
 # EPhone_password_fake_-fixation
 pandavn路由器闪讯密码伪固定(通过一个android手机实现)
+
+目前正在进行中.已经有思路了.
---
+++ test.sh
@@ -0,0 +1,31 @@
+#!/bin/bash
+#解析json数据
+function get_json_value()
+{
+  local json=$1
+  local key=$2
+
+  if [[ -z "$3" ]]; then
+    local num=1
+  else
+    local num=$3
+  fi
+
+  local value=$(echo "${json}" | awk -F"[,:}]" '{for(i=1;i<=NF;i++){if($i~/'${key}'\042/){print $(i+1)}}}' | tr -d '"' | sed -n ${num}p)
+
+  echo ${value}
+}
+
+
+
+# server_back=$(curl -s https://api.myjson.com/bins/15u89e)
+server_back="{\"next_update_time\": \"2019-09-09\",\"new_password\": 762321}"
+# 获取服务器的新密码
+new_password=$(get_json_value $server_back new_password)
+# 获取过期时间
+next_update_time=$(get_json_value $server_back next_update_time)
+echo $new_password
+echo $next_update_time
+
+
+#通过查询nvram当中的登录密码.
